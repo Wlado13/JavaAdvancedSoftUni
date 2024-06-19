3d220c513c41abc2b1e5518f6a5b95aa8c8c9514
Vladimir Vladimirov <99030338+Wlado13@users.noreply.github.com>
2024-06-19 03:24:10 +0300

    Add files via upload

---
+++ _3_MultidimensionalArrays/_4_SumMatrixElements.java
@@ -0,0 +1,11 @@
+package _3_MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class _4_SumMatrixElements {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
